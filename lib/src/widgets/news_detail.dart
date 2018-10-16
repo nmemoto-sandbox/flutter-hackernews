d82493c25560fa33db75dcf65fb669eb2917f9ad
Takafumi Umemoto <umecch@gmail.com>
2018-10-17 06:17:17 +0900

    ナビゲーションを実装

---
+++ lib/src/widgets/news_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsDetail extends StatelessWidget {
+
+  final int itemId;
+
+  NewsDetail({this.itemId});
+  
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: Text('$itemId')
+    );
+  }
+}
