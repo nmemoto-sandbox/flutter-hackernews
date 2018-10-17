f1ace3611f00cc554a42b8daf8dd5b172415eb84
Takafumi Umemoto <umecch@gmail.com>
2018-10-17 22:56:54 +0900

    Itemに紐づくCommentsの取得をBlocパターンで実装

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,14 +3,17 @@ import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
 import 'screens/news_list.dart';
 import 'widgets/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News',
-        onGenerateRoute: routes,
-      ),
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News',
+          onGenerateRoute: routes,
+        ),
+      )
     );
   }
 
@@ -24,7 +27,11 @@ class App extends StatelessWidget {
     } else {
       return MaterialPageRoute(
         builder: (context) {
+          final commentsBloc = CommentsProvider.of(context);
           final itemId = int.parse(settings.name.replaceFirst('/', ''));
+          
+          commentsBloc.fetchItemWithComments(itemId);
+          
           return NewsDetail(
             itemId: itemId,
           );
