0fc0fac0f3915c4a351fda8298ded431ec9567e2
Takafumi Umemoto <umecch@gmail.com>
2018-10-19 21:39:05 +0900

    エスケープ文字の見た目を修正

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -21,7 +21,7 @@ class Comment extends StatelessWidget {
 
         final children = <Widget>[
           ListTile(
-            title: Text(item.text),
+            title: buildText(item),
             subtitle: item.by == "" ? Text("Deleted"): Text(item.by),
             contentPadding: EdgeInsets.only(
               right: 16.0,
@@ -42,5 +42,16 @@ class Comment extends StatelessWidget {
         );
       },
     );
+  }
+
+  Widget buildText(ItemModel item) {
+    final text = item.text
+      .replaceAll('&#x27;', "'")
+      .replaceAll('<p>', '\n\n')
+      .replaceAll('</p>', '')
+      .replaceAll('&#x2F;', "/")
+      .replaceAll('&quot;', '"')
+      ;
+    return Text(text);
   }
 }
